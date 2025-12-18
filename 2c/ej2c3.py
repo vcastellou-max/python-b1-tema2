b9a12857ad95f311427f72030fca7fd0a67ac82a
vcastellou-max <vcastellou@uoc.edu>
2025-12-18 16:43:44 +0100

    Update ej2c3.py

--- 2c/ej2c3.py
+++ 2c/ej2c3.py
@@ -78,7 +78,7 @@ def calculate_factorial(number: int):
     try:
         return factorial(number)
     except Exception as error:
-        return "An unexpected error has occurred: {error}"
+        return f"An unexpected error has occurred: {error}"
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
