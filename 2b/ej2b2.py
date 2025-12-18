5660d3ca55f81f9372e1155e751912436606b374
vcastellou-max <vcastellou@uoc.edu>
2025-12-18 16:06:49 +0100

    Update ej2b2.py

--- 2b/ej2b2.py
+++ 2b/ej2b2.py
@@ -60,10 +60,14 @@ L'any a comprovar serà '2000', de manera que:
 """
 
 
-def check_leap_year(
-    year):
-    # Write here your code
-    pass
+def check_leap_year(year):
+    if (year % 4 == 0 and year % 100 =! 0) or (
+      year % 4 == 0 and year % 100 == 0 and year % 400 == 0
+    ):
+      return True
+    else:
+      return False
+      
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el
